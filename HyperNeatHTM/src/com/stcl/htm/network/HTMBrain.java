dd93e0ed6536f9c50c07c48d74a288e58fe0c420
Simon <Simon@tcpost.dk>
2015-04-03 13:02:07 +0300

    Create wrapper around HTM Network tu use with AHNI

---
+++ HyperNeatHTM/src/com/stcl/htm/network/HTMBrain.java
@@ -0,0 +1,125 @@
+package com.stcl.htm.network;
+
+import java.awt.Graphics2D;
+
+import stcl.algo.brain.Network;
+
+import com.anji.integration.Activator;
+
+public class HTMBrain extends Network implements Activator {
+
+	@Override
+	public String getXmlRootTag() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getXmld() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double[] next(double[] stimuli) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double[][] nextSequence(double[][] stimuli) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double[][] next(double[][] stimuli) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double[][][] nextSequence(double[][][] stimuli) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String toXml() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double getMinResponse() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getMaxResponse() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int[] getInputDimension() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int[] getOutputDimension() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getInputCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getOutputCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean render(Graphics2D g, int width, int height, int neuronSize) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean isRecurrent() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
