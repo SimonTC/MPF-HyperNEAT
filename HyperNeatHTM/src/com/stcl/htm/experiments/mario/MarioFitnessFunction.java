fbbbc6dc73d53fb458cd26ff9426a8e3175e8d11
Simon <Simon@tcpost.dk>
2015-04-10 13:17:48 +0300

    Some chagnges to fitness function

--- HyperNeatHTM/src/com/stcl/htm/experiments/mario/MarioFitnessFunction.java
+++ HyperNeatHTM/src/com/stcl/htm/experiments/mario/MarioFitnessFunction.java
@@ -23,7 +23,7 @@ public class MarioFitnessFunction extends HyperNEATFitnessFunction {
 	private String[] levelParameters;
 	private int numLevels = 10; //TODO: Take from parameter
 	private int difficulty = 2; //TODO: Should grow the better the agents are
-	private int numTrainingLevels = 5;
+	private int numTrainingLevels = 20;
 	private int levelLength = 256;
 	private String agentName = "Scanner";
 	private ArrayList<SimpleMatrix> actions;
@@ -49,7 +49,8 @@ public class MarioFitnessFunction extends HyperNEATFitnessFunction {
 	private void createLevels(){
 		levelParameters = new String[numLevels];
 		//String base = "-vis off -lb on -lca on -lco on -lde on -lf off -lg on -lhs on -ltb on -ll " + levelLength;
-		String base = "-vis off -lb off -lca off -lco off -lde off -le off -lf off -lg off -lhs off -ltb off -ll " + levelLength;
+		//String base = "-vis off -lb off -lca off -lco off -lde off -le off -lf off -lg off -lhs off -ltb off -ll " + levelLength;
+		String base = "-vis off -lb on -lca off -lco off -lde off -le off -lf off -lg off -lhs off -ltb off -ll " + levelLength;
 		for (int i = 0; i < numLevels; i++){
 			String s = base + " -ls " + rand.nextInt(100);
 			levelParameters[i] = s;
